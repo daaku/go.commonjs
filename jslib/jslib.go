f826db6da3c354882ef1b34177b2868b5df16b68
Naitik Shah <n@daaku.org>
2013-02-03 12:17:52 -0800

    initial helper jslib

---
+++ jslib/jslib.go
@@ -0,0 +1,13 @@
+package jslib
+
+import (
+	"github.com/daaku/go.commonjs"
+)
+
+var JQuery_1_8_2 = commonjs.NewURLModule(
+	"jquery",
+	"http://code.jquery.com/jquery-1.8.2.min.js")
+
+var Bootstrap_2_2_2 = commonjs.NewURLModule(
+	"bootstrap",
+	"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/bootstrap.min.js")
