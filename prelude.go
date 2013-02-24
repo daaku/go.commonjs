6d853a8c2ea2786022fc26ceb0aacc92f1319349
Naitik Shah <n@daaku.org>
2013-02-24 10:25:21 -0800

    semi colons as appropriate

--- prelude.go
+++ prelude.go
@@ -5,22 +5,22 @@ const prelude = `
   var _payloads = {},
       _modules = {},
       _execute = [],
-      _schedule = null
+      _schedule = null;
 
   function key(name) {
-    return '_n_' + name
+    return '_n_' + name;
   }
 
   function run() {
-    var current = _execute
-    _execute = []
+    var current = _execute;
+    _execute = [];
     for (var i=0, l=current.length; i<l; i++) {
       var c = current[i],
-          k = key(c.module)
+          k = key(c.module);
       if (_modules[k] || _payloads[k]) {
-        require(c.module)[c.fn].apply(null, c.args)
+        require(c.module)[c.fn].apply(null, c.args);
       } else {
-        execute(c)
+        execute(c);
       }
     }
   }
@@ -29,45 +29,50 @@ const prelude = `
     if (!_schedule) {
       _schedule = window.setTimeout(
         function() {
-          _schedule = null
-          run()
+          _schedule = null;
+          run();
         },
         0
-      )
+      );
     }
   }
 
   function execute(c) {
-    _execute.push(c)
-    schedule()
+    _execute.push(c);
+    schedule();
   }
 
   function require(name) {
     var k = key(name),
-        m = _modules[k]
+        m = _modules[k];
 
-    if (m) return m.exports
+    if (m) {
+      return m.exports;
+    }
 
-    var fn = _payloads[k]
-    if (!fn) throw 'module ' + name + ' not found'
-    ;delete _payloads[k]
-    fn = new Function('require', 'exports', 'module', fn)
-    _modules[k] = m = { name: name, exports: {} }
-    fn.call(exports, require, m.exports, m)
-    return m.exports
+    var fn = _payloads[k];
+    if (!fn) {
+      throw 'module ' + name + ' not found';
+    }
+    delete _payloads[k];
+    fn = new Function('require', 'exports', 'module', fn);
+    _modules[k] = m = { name: name, exports: {} };
+    fn.call(exports, require, m.exports, m);
+    return m.exports;
   }
 
   function define(name, payload) {
-    var k = key(name)
-    if (k in _payloads || k in _modules)
-      throw 'module ' + name + ' already defined'
-    _payloads[k] = payload
-    schedule()
+    var k = key(name);
+    if (k in _payloads || k in _modules) {
+      throw 'module ' + name + ' already defined';
+    }
+    _payloads[k] = payload;
+    schedule();
   }
 
-  exports.define = define
-  exports.require = require
-  exports.execute = execute
+  exports.define = define;
+  exports.require = require;
+  exports.execute = execute;
 })(this);
 `
 
